bb8353ab7af2dddb6e3ea34372aaa65679cfe7df
AniMurthy <122012546+AniMurthy@users.noreply.github.com>
2023-04-19 22:33:49 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ import random
 
 st.header("TDS Week 8: Largest Among 3")
 st.write("#### Enter 3 numbers")
-st.write("Submitted by: Ashwin Hebbar (21f1003155)")
+st.write("Submitted by: Anirudh Murthy (21f1000931)")
 st.write("PS: A random number between $$1$$ and $$100$$ is generated for each number by default")
 st.write("---")
 a = st.number_input("Enter 1st number", value=random.randint(1,100))
 b = st.number_input("Enter 2nd number", value=random.randint(1,100))
 c = st.number_input("Enter 3rd number", value=random.randint(1,100))
-
-
 
 calc = st.button("Find Largest")
 if calc:
